9bfef5095b9282eb8166ed56fbdf097e6adf78b6
Matijs van Zuijlen <matijs@matijs.net>
2012-09-14 18:30:35 +0200

    Move test for Pango::Context#get_font_map from gir_ffi

---
+++ test/end_to_end/context_test.rb
@@ -0,0 +1,11 @@
+require File.expand_path('../test_helper.rb', File.dirname(__FILE__))
+
+describe Pango::Context do
+  describe "#get_font_map" do
+    it "returns a descendant from Pango::FontMap" do
+      ctx = Gdk.pango_context_get
+      result = ctx.get_font_map
+      result.class.ancestors.must_include Pango::FontMap
+    end
+  end
+end

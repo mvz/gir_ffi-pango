e18e01b3959082ecee99baa7f1c020aca4adcbd4
Matijs van Zuijlen <matijs@matijs.net>
2021-11-26 12:26:41 +0100

    Move language overrides file
    This file doesn't do much at the moment but this keeps its location
    consistent with the font overrides.

--- lib/gir_ffi-pango.rb
+++ lib/gir_ffi-pango.rb
@@ -5,5 +5,5 @@ require "gir_ffi"
 GirFFI.setup :Pango
 
 require "gir_ffi-pango/font_overrides"
-require "gir_ffi-pango/language"
+require "gir_ffi-pango/language_overrides"
 require "gir_ffi-pango/version"

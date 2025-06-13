fff8eae59266806b9580bd6fe8dda800341f004e
Matijs van Zuijlen <matijs@matijs.net>
2025-06-13 16:48:33 +0200

    Autocorrect Minitest/EmptyLineBeforeAssertionMethods

--- test/unit/language_test.rb
+++ test/unit/language_test.rb
@@ -5,6 +5,7 @@ require File.expand_path("../test_helper.rb", File.dirname(__FILE__))
 describe Pango::Language do
   it "creates an instance with .from_string" do
     lang = Pango::Language.from_string "en"
+
     _(lang).wont_be_nil
     _(lang).must_be_instance_of Pango::Language
   end
@@ -13,6 +14,7 @@ describe Pango::Language do
     it "returns an enumerable of symbols representing scripts" do
       lang = Pango::Language.from_string "ja"
       scripts = lang.get_scripts
+
       _(scripts.to_a).must_equal [:han, :katakana, :hiragana]
     end
 
@@ -20,9 +22,11 @@ describe Pango::Language do
       lang = Pango::Language.from_string "en"
 
       result = lang.get_scripts
+
       _(result.to_a).must_equal [:latin]
 
       result = lang.get_scripts
+
       _(result.to_a).must_equal [:latin]
     end
   end
@@ -31,6 +35,7 @@ describe Pango::Language do
     it "returns an enumerable of symbols representing scripts" do
       lang = Pango::Language.from_string "ja"
       scripts = lang.scripts
+
       _(scripts.to_a).must_equal [:han, :katakana, :hiragana]
     end
 
@@ -38,9 +43,11 @@ describe Pango::Language do
       lang = Pango::Language.from_string "en"
 
       result = lang.scripts
+
       _(result.to_a).must_equal [:latin]
 
       result = lang.scripts
+
       _(result.to_a).must_equal [:latin]
     end
   end
